6fe8fb2606e8e2812989b87984a04ddcedf9014b
Gildo <1gildojunior@gmail.com>
2023-01-20 03:57:34 -0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Theme } from './config/Theme'
-import 'boxicons';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
