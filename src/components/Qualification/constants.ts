274ac84cf2d59fb72b5711ff1c78b598330c4667
Gildo <1gildojunior@gmail.com>
2023-01-26 13:29:13 -0300

    Fix country name

--- src/components/Qualification/constants.ts
+++ src/components/Qualification/constants.ts
@@ -2,42 +2,42 @@ export const EXPERIENCE = [
   {
     title: "Desenvolvedor Full Stack Pleno",
     organization: "Omie",
-    country: "Brazil",
+    country: "Brasil",
     startDate: "Agosto 2022",
     endDate: "Present"
   },
   {
     title: "Desenvolvedor Full Stack Pleno",
     organization: "Exact Sales",
-    country: "Brazil",
+    country: "Brasil",
     startDate: "Janeiro 2021",
     endDate: "Agosto 2022"
   },
   {
     title: "Desenvolvedor Full Stack Junior",
     organization: "Exact Sales",
-    country: "Brazil",
+    country: "Brasil",
     startDate: "Fevereiro 2020",
     endDate: "Janeiro 2021"
   },
   {
     title: "Analista de Suporte Técnico",
     organization: "Exact Sales",
-    country: "Brazil",
+    country: "Brasil",
     startDate: "Dezembro 2019",
     endDate: "Fevereiro 2020"
   },
   {
     title: "Assistente de Suporte Técnico",
     organization: "Exact Sales",
-    country: "Brazil",
+    country: "Brasil",
     startDate: "Agosto 2019",
     endDate: "Dezembro 2019"
   },
   {
     title: "Estágiario de Suporte Técnico",
     organization: "Exact Sales",
-    country: "Brazil",
+    country: "Brasil",
     startDate: "Janeiro 2019",
     endDate: "Agosto 2019"
   },
@@ -47,7 +47,7 @@ export const EDUCATION = [
   {
     title: "Análise e Desenvolvimento de Sistemas",
     organization: "Faculdade Cesusc",
-    country: "Brazil",
+    country: "Brasil",
     startDate: "Fevereiro 2017",
     endDate: "Dezembro 2019"
   },
