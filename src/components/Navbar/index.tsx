07249269df8b4f3e51ae5969c78a82d14d1e150c
Gildo <1gildojunior@gmail.com>
2023-01-28 15:45:11 -0300

    navbar mobile comportment

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import useReactPath from "../../hooks/useReactPath";
 import { getNavigation } from "../constants";
 import { Divider } from "../_UI/Divider";
-import { Container, Link, NavbarButton, NavbarIcon, StyledList, StyledNav, BackgroundMask } from "./styles";
+import { Container, Link, NavbarButton, NavbarIcon, StyledList, StyledNav } from "./styles";
 
 export function Navbar() {
   const urlPath = useReactPath();
@@ -13,20 +13,18 @@ export function Navbar() {
   }, [urlPath]);
 
   return (
-    <>
-      <Container>
-        <StyledNav>
-          <NavbarButton onClick={() => setOpen(!open)}>
-            <NavbarIcon className='bx bx-menu'></NavbarIcon>
-          </NavbarButton>
-          <StyledList open={open}>
-            {getNavigation(urlPath).map(({ id, href, text, active }) => (
-              <li key={id}><Link active={active} open={open} href={href}>{text}</Link></li>
-            ))}
-          </StyledList>
-        </StyledNav >
-        <Divider />
-      </Container >
-    </>
+    <Container>
+      <StyledNav>
+        <NavbarButton onClick={() => setOpen(!open)}>
+          <NavbarIcon className='bx bx-menu'></NavbarIcon>
+        </NavbarButton>
+        <StyledList open={open}>
+          {getNavigation(urlPath).map(({ id, href, text, active }) => (
+            <li key={id}><Link active={active} open={open} href={href}>{text}</Link></li>
+          ))}
+        </StyledList>
+      </StyledNav >
+      <Divider />
+    </Container >
   );
 }
