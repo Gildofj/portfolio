99f4d5ecb69267d3301070d6f0a966ba6da7aaab
Gildo <1gildojunior@gmail.com>
2023-02-03 20:48:58 -0300

    Education Qualifications

--- src/components/Qualification/constants.ts
+++ src/components/Qualification/constants.ts
@@ -45,6 +45,48 @@ export const EXPERIENCE = [
 
 export const EDUCATION = [
   {
+    title: "Kubernetes Bootcamp",
+    organization: "KubDev.io",
+    country: "Brasil",
+    startDate: "Maio 2021",
+    endDate: "Maio 2021"
+  },
+  {
+    title: "Bootcamp Alta Disponibilidade em Cloud",
+    organization: "Cloud Treinamentos",
+    country: "Brasil",
+    startDate: "Outubro 2020",
+    endDate: "Outubro 2020"
+  },
+  {
+    title: "C# Completo",
+    organization: "Udemy",
+    country: "Brasil",
+    startDate: "Fevereiro 2020",
+    endDate: "Fevereiro 2020"
+  },
+  {
+    title: "Node with React: Fullstack Web Development",
+    organization: "Udemy",
+    country: "Brasil",
+    startDate: "Outubro 2019",
+    endDate: "Outubro 2019"
+  },
+  {
+    title: "Linguagem de Programação Java - Básico",
+    organization: "Fundação Bradesco",
+    country: "Brasil",
+    startDate: "Janeiro 2019",
+    endDate: "Janeiro 2019"
+  },
+  {
+    title: "Introdução à Programação Orientada a Objetos (POO)",
+    organization: "Fundação Bradesco",
+    country: "Brasil",
+    startDate: "Dezembro 2018",
+    endDate: "Dezembro 2018"
+  },
+  {
     title: "Análise e Desenvolvimento de Sistemas",
     organization: "Faculdade Cesusc",
     country: "Brasil",
