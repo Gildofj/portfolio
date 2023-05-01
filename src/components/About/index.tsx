250de449ff405c0cdce08bd6b242da5974b78870
Gildo <1gildojunior@gmail.com>
2023-05-01 01:41:58 -0300

    Change about

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -37,7 +37,7 @@ export function About() {
 
       <AboutContainer>
         <Title>Sobre mim</Title>
-        <AboutText>Sou formado em Análise e Desenvolvimento de Sistemas e atuo na área de desenvolvimento desde o inicio de 2020 porém estudo programação desde 2017, atualmente trabalho diretamente com C# com .NET Core, Javascript e Typescript como linguagem de programação, mais especificamente com frameworks como AngularJS, ReactJS e NextJS além de utilizar Kotlin para mobile e SQL Server como banco de dados, porém tenho conhecimento em ExpressJS como backend em node e React Native como mobile. Atualmente estou estudando Docker, além de estudar conceitos de arquitetura de projetos como Clean Architecture e DDD.</AboutText>
+        <AboutText>Sou manezinho da ilha nascido e criado em Florianópolis, Santa Catarina, me formei em Análise e Desenvolvimento de Sistemas e atuo na área de desenvolvimento desde o inicio de 2020 porém estudo programação desde 2017, atualmente trabalho diretamente com C# com .NET Core, Javascript e Typescript como linguagem de programação mais especificamente com frameworks como AngularJS, ReactJS e NextJS além de Kotlin para mobile e SQL Server como banco de dados, porém tenho conhecimento em ExpressJS como backend em node e React Native como mobile. Atualmente estou estudando Docker, além de estudar conceitos de arquitetura de projetos como Clean Architeture e DDD.</AboutText>
       </AboutContainer>
     </Container>
   );
