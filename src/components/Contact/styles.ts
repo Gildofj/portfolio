a6d9961c4fe403320ec273a3674bcb7513264cfc
Gildo Junior <1gildojunior@gmail.com>
2023-05-01 18:46:07 -0300

    Ajust layout contact types

--- src/components/Contact/styles.ts
+++ src/components/Contact/styles.ts
@@ -43,7 +43,7 @@ export const CardContact = styled.div`
   align-items: center;
   border-radius: 0.625rem;
   gap: 0.5rem;
-  padding: 1rem 3rem;
+  padding: 1.2rem 3rem;
   background: rgb(114,14,158);
   background: -moz-linear-gradient(150deg, rgba(114,14,158,1) 0%, rgba(128,0,128,1) 100%);
   background: -webkit-linear-gradient(150deg, rgba(114,14,158,1) 0%, rgba(128,0,128,1) 100%);
