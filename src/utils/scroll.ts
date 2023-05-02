569efe71a82b6f3807a837fb866f0dd5bf6bd92d
Gildo Junior <1gildojunior@gmail.com>
2023-05-01 23:07:48 -0300

    fix scroll when overlay is opened

--- src/utils/scroll.ts
+++ src/utils/scroll.ts
@@ -1,6 +1,9 @@
 export function handleScrollWhenModalIsOpen(open: boolean) {
-  if (open)
-    document.body.style.overflow = "hidden";
-  else
-    document.body.style.overflow = "unset";
+  if (open) {
+    document.body.style.overflow = 'hidden';
+    document.body.style.touchAction = 'none';
+  } else {
+    document.body.style.overflow = 'auto';
+    document.body.style.touchAction = 'auto';
+  }
 }
