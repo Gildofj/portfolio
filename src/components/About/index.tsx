58b2efeb549bf1d92afcae03380d49514b564d1a
Gildo Junior <1gildojunior@gmail.com>
2023-12-11 00:28:59 -0300

    refatoracao

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -14,8 +14,12 @@ export function About({ }: AboutProps) {
       <TitleContainer>
         <Title>Sobre mim</Title>
       </TitleContainer>
-      <AboutText>Eu sou natural de Florianópolis, Santa Catarina, e tenho formação em Análise e Desenvolvimento de Sistemas. Atuo na área de desenvolvimento desde o início de 2020, mas tenho estudado programação desde 2017. Atualmente, meu trabalho envolve principalmente a utilização de C# com .NET Core, além de Javascript e Typescript para o desenvolvimento de aplicações web e móveis. Eu sou experiente em frameworks como AngularJS, ReactJS e NextJS, e também tenho conhecimentos em Kotlin para o desenvolvimento de aplicativos móveis e SQL Server como banco de dados. Além disso, possuo habilidades em ExpressJS como backend em node e React Native como mobile.</AboutText>
-      <AboutText>Durante o processo de estudo, tenho explorado o uso de Docker, bem como conceitos importantes em arquitetura de projetos, como Clean Architecture e Domain-Driven Design (DDD).</AboutText>
+      <AboutText>
+        Eu sou natural de Florianópolis, Santa Catarina, e tenho formação em Análise e Desenvolvimento de Sistemas. Atuo na área de desenvolvimento desde o início de 2020, mas tenho estudado programação desde 2017. Atualmente, meu trabalho envolve principalmente a utilização de C# com .NET Core, além de Javascript e Typescript para o desenvolvimento de aplicações web e móveis. Eu sou experiente em frameworks como AngularJS, ReactJS e NextJS, e também tenho conhecimentos em Kotlin para o desenvolvimento de aplicativos móveis e SQL Server como banco de dados. Além disso, possuo habilidades em ExpressJS como backend em node e React Native como mobile.
+      </AboutText>
+      <AboutText>
+        Durante o processo de estudo, tenho explorado o uso de Docker, bem como conceitos importantes em arquitetura de projetos, como Clean Architecture e Domain-Driven Design (DDD).
+      </AboutText>
     </Container>
   );
 }
