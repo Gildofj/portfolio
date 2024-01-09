17f8d50081055e6af2d0843ccc91797ba1db381d
Gildo Junior <1gildojunior@gmail.com>
2024-01-09 02:10:35 -0300

    fix about description

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -15,13 +15,12 @@ export function About({}: AboutProps) {
         Eu sou natural de Florianópolis, Santa Catarina, e tenho formação em
         Análise e Desenvolvimento de Sistemas. Atuo na área de desenvolvimento
         desde o início de 2020, mas tenho estudado programação desde 2017.
-        Atualmente, meu trabalho envolve principalmente a utilização de C# com
-        .NET Core, além de Javascript e Typescript para o desenvolvimento de
-        aplicações web e móveis. Eu sou experiente em frameworks como AngularJS,
-        ReactJS e NextJS, e também tenho conhecimentos em Kotlin para o
-        desenvolvimento de aplicativos móveis e SQL Server como banco de dados.
-        Além disso, possuo habilidades em ExpressJS como backend em node e React
-        Native como mobile.
+        Atualmente, meu trabalho envolve principalmente a utilização de Kotlin,
+        C# com .NET Core, além de Javascript e Typescript para o desenvolvimento
+        de aplicações web e móveis. Eu sou experiente em frameworks como
+        AngularJS, ReactJS e NextJS, e também tenho conhecimentos SQL Server
+        como banco de dados. Além disso, possuo habilidades em ExpressJS como
+        backend em node e React Native como mobile.
       </AboutText>
       <AboutText>
         Durante o processo de estudo, tenho explorado o uso de Docker, bem como
