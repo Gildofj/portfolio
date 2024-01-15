60001b535b2f9ebbefbd8592b7588eb5787dcf91
Gildo Junior <1gildojunior@gmail.com>
2024-01-14 21:39:02 -0300

    fix title component and about

--- src/components/About/styles.ts
+++ src/components/About/styles.ts
@@ -4,21 +4,13 @@ export const Container = styled.section`
   width: 100%;
   min-height: 100vh;
   display: flex;
-  max-width: 90rem;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   gap: 1rem;
-  padding: 7rem 2rem;
 `;
 
 export const AboutText = styled.p`
-  color: ${({ theme: { colors } }) => colors.text};
-  width: 100%;
-  line-height: 2rem;
+  text-indent: 1rem;
   text-align: justify;
-
-  @media screen and (max-width: 768px) {
-    font-size: 1.25rem;
-  }
 `;
--- src/components/_UI/Title.ts
+++ src/components/_UI/Title.ts
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const Title = styled.h3`
   font-size: 2rem;
   font-weight: bold;
-  color: ${({ theme: { colors } }) => colors.pink};
+  color: #a855f7;
+  font-family: "Fira Code";
 
   @media screen and (max-width: 768px) {
     font-size: 2.25rem;
