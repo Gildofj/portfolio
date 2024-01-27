637375fe36fb9bb87b10cdcdcae25bb7da0632af
Gildo Junior <1gildojunior@gmail.com>
2024-01-27 16:00:35 -0300

    fix skills title

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,3 +1,4 @@
+import { useIntl } from "react-intl";
 import { Title } from "../_UI/Title";
 import { TitleContainer } from "../_UI/TitleContainer";
 import { ScrollAnimatedProps } from "../types";
@@ -8,12 +9,13 @@ import { useSkills } from "./useSkills";
 interface SkillsProps extends ScrollAnimatedProps {}
 
 export function Skills({}: SkillsProps) {
+  const intl = useIntl();
   const { skills } = useSkills();
 
   return (
     <Container id="skills">
       <TitleContainer>
-        <Title>Skills</Title>
+        <Title>{intl.formatMessage({ id: "skills.title" })}</Title>
       </TitleContainer>
       <Grid>
         {skills?.map((skill, i) => (
