18a045d4b38b37ea9fa47b07c94c7fd8ebd041a1
Gildo Junior <1gildojunior@gmail.com>
2026-02-08 00:15:44 -0300

    feat: integrate Sentry for error tracking and monitoring
    - Updated emailjs service and template IDs to use NEXT_PUBLIC environment variables.
    - Added Sentry configuration files for edge and server environments.
    - Created a sample API route to test Sentry's error monitoring.
    - Implemented a global error handler to capture exceptions with Sentry.
    - Generated a root layout file for Sentry integration.
    - Developed a sample page to demonstrate Sentry functionality, including error throwing and connectivity checks.
    - Configured client-side Sentry initialization with Replay integration and sampling rates.

--- src/sections/Contact/index.tsx
+++ src/sections/Contact/index.tsx
@@ -59,8 +59,8 @@ export function Contact() {
 
       try {
         await emailjs.sendForm(
-          process.env.VITE_EMAILJS_SERVICE_ID!,
-          process.env.VITE_EMAILJS_TEMPLATE_ID!,
+          process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+          process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
           form.current,
         );
 
